24992197e0f51f5c94bebf4b9deb79cde04e9340
John Bytheway <jbytheway@gmail.com>
2009-10-15 22:39:47 +0100

    Start of ai_client (creates AI, but doesn't use it)
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

---
+++ bin/ai_client/main.cpp
@@ -0,0 +1,85 @@
+#include <string>
+#include <iostream>
+
+#include <boost/optional.hpp>
+#include <boost/algorithm/string/join.hpp>
+
+#include <optionsparser.h>
+
+#include <konig/tableposition.hpp>
+#include <konig/ai/ai.hpp>
+#include <konig/ai/nosuchai.hpp>
+
+namespace konig { namespace ai_client {
+
+struct Options {
+  Options() :
+    help(false)
+  {}
+  bool help;
+  boost::optional<int> position;
+  boost::optional<std::string> description;
+};
+
+void usage(std::ostream& o)
+{
+  o <<
+"Usage: konig-ai_client -h\n"
+"       konig-ai_client -p POSITION -a DESCRIPTION\n"
+"  -h, --help    Display this message.\n"
+"  -p, --position POSITION\n"
+"                Join table at position POSITION (1--4)\n"
+"  -a, --ai DESCRIPTION\n"
+"                Run AI described by DESCRIPTION\n"
+  << std::flush;
+}
+
+}}
+
+int main(int const argc, char const* const* const argv)
+{
+  konig::ai_client::Options options;
+  optimal::OptionsParser parser;
+  parser.addOption("help",     'h', &options.help);
+  parser.addOption("position", 'p', &options.position);
+  parser.addOption("ai",       'a', &options.description);
+
+  if (parser.parse(argc, argv)) {
+    std::cerr << "options parsing failed\n" <<
+      boost::algorithm::join(parser.getErrors(), "\n") << '\n' << std::endl;
+    konig::ai_client::usage(std::cerr);
+    return EXIT_FAILURE;
+  }
+
+  if (options.help) {
+    konig::ai_client::usage(std::cerr);
+    return EXIT_SUCCESS;
+  }
+
+  if (!options.description) {
+    std::cerr << "must provide AI description\n\n";
+    konig::ai_client::usage(std::cerr);
+    return EXIT_FAILURE;
+  }
+
+  if (!options.position) {
+    std::cerr << "must provide table position\n\n";
+    konig::ai_client::usage(std::cerr);
+    return EXIT_FAILURE;
+  }
+
+  if (*options.position < 1 || *options.position >= konig::TablePosition::max) {
+    std::cerr << "position invalid\n";
+    return EXIT_FAILURE;
+  }
+
+  try {
+    konig::ai::Ai::Ptr ai = konig::ai::Ai::create(*options.description);
+  } catch (konig::ai::AiError const& e) {
+    std::cerr << "error creating AI: " << e.what() << "\n";
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
