ef57795e1fc2dafe63845210650679c3fa347cef
John Bytheway <jbytheway@gmail.com>
2009-10-24 18:10:49 +0100

    Better parsing of Announcements
    - Allow defensive announcements (with 'd' or '!')
    - Determine correct Announcedness
    
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- konig/announcement.cpp
+++ konig/announcement.cpp
@@ -6,16 +6,26 @@ bool Announcement::from_string(Announcement& a, std::string const& d)
 {
   if (d.size() == 0) return false;
   size_t p = d.size();
-  a.announcedness = Announcedness::unannounced;
+  a.announcedness = Announcedness::announced;
   while (p != 0 && d[p-1] == 'x' &&
       a.announcedness != Announcedness::subkontraed) {
     --p;
     a.announcedness = a.announcedness.next_level();
   }
-  if (p > 1) return false;
-  if (p == 0) {
+  if (p > 2) {
+    return false;
+  } else if (p == 0) {
     a.feat = Feat::game;
+    a.defensive = false;
     return true;
+  } else if (p == 2) {
+    if (d[1] == 'd' || d[1] == '!') {
+      a.defensive = true;
+    } else {
+      return false;
+    }
+  } else {
+    a.defensive = false;
   }
   switch (d[0]) {
     case '1':
