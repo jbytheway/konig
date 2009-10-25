13b5156a07e57bdc5eb4c6386db35427f3478430
John Bytheway <jbytheway@gmail.com>
2009-10-25 16:57:14 +0000

    Rearranged a bidding test and finish it (it fails)
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- konig/ai/test/bidding.cpp
+++ konig/ai/test/bidding.cpp
@@ -2,27 +2,38 @@
 
 #include <konig/biddingsequence.hpp>
 #include <konig/ai/specificplayai.hpp>
+#include <konig/ai/invalidplayerror.hpp>
 
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
 using namespace konig;
+using boost::assign::list_of;
+
+namespace {
+  void do_bid_test(std::vector<std::string> const& bid_names)
+  {
+    Ruleset rules = Ruleset::cheltenham();
+    Contracts const& contracts = rules.contracts();
+    BiddingSequence bs(contracts);
+    std::array<std::vector<Bid>, 4> bids;
+    for (size_t i=0; i<bid_names.size(); ++i) {
+      bids[i%4].push_back(contracts.index_by_bid_name(bid_names[i]));
+    }
+    std::vector<Player::Ptr> players;
+    BOOST_FOREACH(auto& bid, bids) {
+      players.push_back(Player::Ptr(new ai::SpecificPlayAi(std::move(bid))));
+    }
+    // Fake the game start
+    for (PlayPosition p = position_forehand; p != position_max; ++p) {
+      players[p]->start_game(rules, p, Cards());
+    }
+    bs.get_bids(players);
+  }
+}
 
 BOOST_AUTO_TEST_CASE(bidding_failures)
 {
-  Ruleset rules = Ruleset::cheltenham();
-  Contracts const& contracts = rules.contracts();
-
-  {
-    BiddingSequence bs(contracts);
-    std::vector<Player::Ptr> players;
-    players.push_back(Player::Ptr(new ai::SpecificPlayAi(
-            std::vector<Bid>(1, contracts.index_by_bid_name("t"))
-          )));
-    players.push_back(Player::Ptr(new ai::SpecificPlayAi()));
-    players.push_back(Player::Ptr(new ai::SpecificPlayAi()));
-    players.push_back(Player::Ptr(new ai::SpecificPlayAi()));
-    //bs.get_bids(players);
-  }
+  BOOST_CHECK_THROW(do_bid_test(list_of("t")), ai::InvalidPlayError);
 }
 
