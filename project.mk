16c0dba6e320937b95e18fc9fe370916ec965599
John Bytheway <jbytheway@gmail.com>
2009-10-27 22:40:41 +0000

    Switch from local makefiles to makeshift submodule
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

---
+++ project.mk
@@ -0,0 +1,6 @@
+BUILD_CPPFLAGS += \
+	-std=gnu++0x -DPHOENIX_LIMIT=6 -I$(this_srcdir) -I. -I$(top_srcdir)
+# These warnings arise when optimisations are on.  I don't think either of them
+# is genuine
+BUILD_CXXFLAGS += -Wno-uninitialized -Wno-strict-aliasing
+
