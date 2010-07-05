e12f59d77e19c78d611938502a6ba13e5c83544c
John Bytheway <jbytheway@gmail.com>
2010-07-05 22:47:37 +0100

    -Wno-unused for Boost.Parameter's benefit
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

--- project.mk
+++ project.mk
@@ -1,7 +1,7 @@
 BUILD_CPPFLAGS += \
 	-std=c++0x -DPHOENIX_LIMIT=6 -DBOOST_PARAMETER_MAX_ARITY=6 \
 	-I$(this_srcdir) -I. -I$(top_srcdir)
-# These warnings arise when optimisations are on.  I don't think either of them
-# is genuine
-BUILD_CXXFLAGS += -Wno-uninitialized -Wno-strict-aliasing
+# First wo arise when optimisations are on.  I don't think either of them
+# is genuine.  Third is in BOOST_PARAMETER_CONSTRUCTOR macro.
+BUILD_CXXFLAGS += -Wno-uninitialized -Wno-strict-aliasing -Wno-unused
 
