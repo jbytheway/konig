5dd8d9db2eda4d0804fe2f7267e1b11da210232e
John Bytheway <jbytheway@gmail.com>
2011-07-09 20:47:17 +0100

    More simple Cards parser tests

--- konig/test/cards.cpp
+++ konig/test/cards.cpp
@@ -9,6 +9,17 @@ BOOST_AUTO_TEST_CASE(cards_parser)
 {
   BOOST_CHECK_EQUAL(Cards::from_string(""), Cards());
   BOOST_CHECK_EQUAL(Cards::from_string("Sk"), Cards{Card(TrumpRank::skus)});
+  BOOST_CHECK_EQUAL(Cards::from_string("21"), Cards{Card(TrumpRank::mond)});
+  BOOST_CHECK_EQUAL(Cards::from_string("1"), Cards{Card(TrumpRank::pagat)});
+  BOOST_CHECK_EQUAL(
+    Cards::from_string("H:K"), (Cards{{Suit::hearts, SuitRank::king}})
+  );
+  BOOST_CHECK_EQUAL(
+    Cards::from_string("C:t"), (Cards{{Suit::clubs, SuitRank::ten}})
+  );
+  BOOST_CHECK_EQUAL(
+    Cards::from_string("D:3"), (Cards{{Suit::diamonds, SuitRank::three}})
+  );
 }
 
 }
