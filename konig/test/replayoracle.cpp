4e72605e4924478a771036095bb6679066188109
John Bytheway <jbytheway@gmail.com>
2011-08-07 23:51:47 +0100

    Sorted tests

--- konig/test/replayoracle.cpp
+++ konig/test/replayoracle.cpp
@@ -12,6 +12,39 @@ using boost::assign::list_of;
 
 namespace konig {
 
+BOOST_AUTO_TEST_CASE(t_outcomes_possible)
+{
+  Ruleset const rules = Ruleset::cheltenham();
+
+  {
+    ReplayOracle oracle(
+      rules,
+      {"r", "", "", "", "t"},
+      "",
+      {},
+      "",
+      {},
+      {
+        "21", "Sk", "20", "18",
+        "C9", "CQ", "CJ", "CN",
+        "12", "16", "19", "17",
+        "10", "14", "13", "15",
+        " 8", "CK", " 9", "11",
+        "SJ", "St", "S8", "SN",
+        "HK", "H9", " 5", "HQ",
+        " 6", "DN", " 3", "DK",
+        "DQ", "DJ", "Dt", "SK",
+        "Ht", "H7", " 1", "HN",
+        "C8", "S9", "C7", "Ct",
+        "D8", "S7", "D7", "SQ"
+      }
+    );
+    auto result = play_game(rules, oracle);
+    BOOST_CHECK_EQUAL(result.outcome.string(), "t4");
+    BOOST_CHECK(result.scores == list_of(0)(0)(0)(0));
+  }
+}
+
 BOOST_AUTO_TEST_CASE(s_outcomes_possible)
 {
   Ruleset const rules = Ruleset::cheltenham();
@@ -76,39 +109,6 @@ BOOST_AUTO_TEST_CASE(d_outcomes_possible)
     auto result = play_game(rules, oracle);
     BOOST_CHECK_EQUAL(result.outcome.string(), "d");
     BOOST_CHECK(result.scores == list_of(-3)(-3)(9)(-3));
-  }
-}
-
-BOOST_AUTO_TEST_CASE(t_outcomes_possible)
-{
-  Ruleset const rules = Ruleset::cheltenham();
-
-  {
-    ReplayOracle oracle(
-      rules,
-      {"r", "", "", "", "t"},
-      "",
-      {},
-      "",
-      {},
-      {
-        "21", "Sk", "20", "18",
-        "C9", "CQ", "CJ", "CN",
-        "12", "16", "19", "17",
-        "10", "14", "13", "15",
-        " 8", "CK", " 9", "11",
-        "SJ", "St", "S8", "SN",
-        "HK", "H9", " 5", "HQ",
-        " 6", "DN", " 3", "DK",
-        "DQ", "DJ", "Dt", "SK",
-        "Ht", "H7", " 1", "HN",
-        "C8", "S9", "C7", "Ct",
-        "D8", "S7", "D7", "SQ"
-      }
-    );
-    auto result = play_game(rules, oracle);
-    BOOST_CHECK_EQUAL(result.outcome.string(), "t4");
-    BOOST_CHECK(result.scores == list_of(0)(0)(0)(0));
   }
 }
 
