0a99b84fbaf2514e654edb122ec0992ca532166c
John Bytheway <jbytheway@gmail.com>
2012-02-19 23:27:37 +0000

    Alternate model gen using more reliable means
    The old implementation broke down when using too many factors

--- bin/simulator/experiments/fitModel.R
+++ bin/simulator/experiments/fitModel.R
@@ -2,9 +2,13 @@
 
 args = commandArgs(TRUE)
 dataFilename = args[1]
+coeffFilename = args[2]
+residualsFilename = args[3]
 
 data = read.table(dataFilename, header=TRUE)
 colNames = names(data)
 obs = colNames[3:length(colNames)]
 res = lm(as.formula(paste("score~", paste(obs, collapse="+"))), data=data)
-res$coefficients
+write(res$coefficients, file=coeffFilename, ncolumns=length(colNames)+1)
+write(res$residuals, file=residualsFilename, ncolumns=1)
+
