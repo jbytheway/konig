d2d491e952010efbc42b5196812a4a72c2ae22d5
John Bytheway <jbytheway@gmail.com>
2012-02-19 23:37:52 +0000

    Some more standard features

--- konig/ai/feature.cpp
+++ konig/ai/feature.cpp
@@ -11,11 +11,54 @@ struct NumTrumps : Feature {
   }
 };
 
+struct NumKings : Feature {
+  virtual std::string name() const { return "numkings"; }
+  virtual double compute(Cards const& hand) const {
+    return hand.count(SuitRank::king);
+  }
+};
+
+struct NumVoids : Feature {
+  virtual std::string name() const { return "numvoids"; }
+  virtual double compute(Cards const& hand) const {
+    int result = 0;
+    for (Suit s = Suit::min; s < Suit::trumps; ++s) {
+      result += 0 == hand.count(s);
+    }
+    return result;
+  }
+};
+
+struct NumTopTrumps : Feature {
+  virtual std::string name() const { return "numtoptrumps"; }
+  virtual double compute(Cards const& hand) const {
+    int result = 0;
+    TrumpRank r = TrumpRank::skus;
+    while (hand.count(Card(r))) {
+      --r;
+      assert(r >= TrumpRank::pagat);
+      ++result;
+    }
+    return result;
+  }
+};
+
+struct CardPoints : Feature {
+  virtual std::string name() const { return "cardpoints"; }
+  virtual double compute(Cards const& hand) const {
+    return hand.total_card_points();
+  }
+};
+
 std::vector<Feature::Ptr> Feature::default_feature_sequence()
 {
   using konig::utility::make_unique;
   std::vector<Feature::Ptr> result;
   result.push_back(std::make_shared<NumTrumps>());
+  result.push_back(std::make_shared<NumKings>());
+  result.push_back(std::make_shared<NumVoids>());
+  result.push_back(std::make_shared<NumTopTrumps>());
+  result.push_back(std::make_shared<CardPoints>());
   return result;
 }
 
